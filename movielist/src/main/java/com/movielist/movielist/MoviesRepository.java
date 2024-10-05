7a0c54fb04da75bc7994e38ad5a7a131984fcc3d
daskene <daskene29@gmail.com>
2024-10-05 23:04:08 +0100

    Merge remote-tracking branch 'origin/user_repo_jpa' into movie_list

--- movielist/src/main/java/com/movielist/movielist/MoviesRepository.java
+++ movielist/src/main/java/com/movielist/movielist/MoviesRepository.java
@@ -2,16 +2,24 @@ package com.movielist.movielist;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public interface MoviesRepository extends CrudRepository<Movie, Long>
 {
-	List<Movie> findByMoviename(String moviename);
-	
-	List<Movie> findByDirector(String director);
-	
-	List<Movie> findByDateofrelease(String moviename);
+	@Query("SELECT m FROM Movie m WHERE m.moviename = :moviename")
+	List<Movie> findByMoviename(@Param("moviename") String moviename);
+
+	@Query("SELECT m FROM Movie m WHERE m.director = :director")
+	List<Movie> findByDirector(@Param("director") String director);
+
+	@Query("SELECT m FROM Movie m WHERE m.dateofrelease = :dateofrelease")
+	List<Movie> findByDateofrelease(@Param("dateofrelease") String dateofrelease);
+
+	@Query("SELECT m FROM Movie m WHERE m.director = :director AND m.dateofrelease BETWEEN :startYear AND :endYear")
+	List<Movie> findByDirectorAndReleaseYearRange(@Param("director") String director, @Param("startYear") int startYear, @Param("endYear") int endYear);
 
 }
--- movielist/src/main/java/com/movielist/movielist/UserRepository.java
+++ movielist/src/main/java/com/movielist/movielist/UserRepository.java
@@ -1,6 +1,8 @@
 package com.movielist.movielist;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
@@ -8,10 +10,14 @@ import java.util.List;
 @RepositoryRestResource
 public interface UserRepository extends CrudRepository<User, Long> {
 
-    List<User> findByFirstname(String firstname);
-    List<User> findByLastname(String lastname);
+	@Query("SELECT u FROM User u WHERE u.firstname = :firstname")
+	List<User> findByFirstname(@Param("firstname") String firstname);
 
-    //@Query("select u from users")
-//    List<User> findByFirstNameStartsWith(String firstname);
+	@Query("SELECT u FROM User u WHERE u.lastname = :lastname")
+	List<User> findByLastname(@Param("lastname") String lastname);
+
+	@Query("SELECT u FROM User u WHERE u.firstname LIKE %:name% OR u.lastname LIKE %:name% ORDER BY u.id")
+	List<User> findByNameContaining(@Param("name") String name);
+
 
 }
