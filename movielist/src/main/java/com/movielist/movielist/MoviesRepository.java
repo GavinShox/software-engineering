84ad081d84dcbe85f5d34b94af4e894e0252ede7
daskene <daskene29@gmail.com>
2024-10-29 14:30:38 +0000

    Added CrossOrigin Annotations in Movies Repo

--- movielist/src/main/java/com/movielist/movielist/MoviesRepository.java
+++ movielist/src/main/java/com/movielist/movielist/MoviesRepository.java
@@ -6,8 +6,11 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 @RepositoryRestResource
+@CrossOrigin(origins = "*") // Allows requests from all origins
 public interface MoviesRepository extends CrudRepository<Movie, Long>
 {
 	@Query("SELECT m FROM Movie m WHERE m.moviename = :moviename")
