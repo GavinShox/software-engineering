69ab85e4b0dc03c9acd0d3187839bafd7f393286
Gavin Shaughnessy <gavinshaughnessy@gmail.com>
2024-11-13 15:58:50 +0000

    added CRUD functionality to Users controller

--- movielist/src/main/java/com/movielist/web/UserController.java
+++ movielist/src/main/java/com/movielist/web/UserController.java
@@ -1,10 +1,10 @@
 package com.movielist.web;
 
+import com.movielist.movielist.Movie;
 import com.movielist.movielist.User;
 import com.movielist.movielist.UserRepository;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.web.bind.annotation.*;
-
 
 @RestController
 public class UserController
@@ -22,12 +22,29 @@ public class UserController
         return repository.findAll();
     }
     
-    
-	// GET a movie by ID
-    @GetMapping("/user/{id}")
+	// GET a user by ID
+    @GetMapping("/users/{id}")
     public User getUserById(@PathVariable Long id) {
         return repository.findById(id)
-            .orElseThrow(() -> new ResourceNotFoundException("Movie not found with ID: " + id));
+            .orElseThrow(() -> new ResourceNotFoundException("User not found with ID: " + id));
     }
 
+    @PutMapping("/users/create")
+    public User createUser(@RequestBody User newUser) {
+        return repository.save(newUser);
+    }
+
+    @PutMapping("/users/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User updatedUser) {
+        return repository.findById(id).map(user -> {
+            user.setFirstname(updatedUser.getFirstname());
+            user.setLastname(updatedUser.getLastname());
+            return repository.save(user);
+        }).orElseThrow(() -> new ResourceNotFoundException("User not found with ID: " + id));
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
 }
